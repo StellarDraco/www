b3f3c87713eaf3a80e6c21201c3ebbc97eb7dd6f
StellarDraco <kadenjsteele@gmail.com>
2016-11-25 01:08:10 -0500

    hex program

--- hex.php
+++ hex.php
@@ -1,11 +1,20 @@
 <form>
 	<input type="text" name="string"/>
 </form>
-
+<PRE>
 <?php
-	$string = $_GET['string'];
-	
-	for($i=0;$i<strlen($string);$i++){
-		echo $string[$i]." ".(bin2hex ($string[$i]))." ".(hexdec(bin2hex ($string[$i])))."<br/>";
+	if(isset($_GET['string'])){
+		$string = $_GET['string'];
+
+		for($i=0;$i<strlen($string);$i++){
+			echo $string[$i]." ".(bin2hex ($string[$i]))." ".(hexdec(bin2hex ($string[$i])))."<br/>";
+		}
 	}
 ?>
+</PRE>
+
+<table width="100%">
+  <tr>
+    <td align="center"><img src="http://www.theasciicode.com.ar/american-standard-code-information-interchange/ascii-codes-table.png"/></td>
+  </tr>
+</table>
