42d98183ee5630b34e22b574422d5e82c9337dd4
Kaden Steele <kadenjsteele@gmail.com>
2016-11-28 13:41:11 -0800

    Update hex.php

--- hex.php
+++ hex.php
@@ -18,8 +18,8 @@
     <td align="center"><img src="http://www.theasciicode.com.ar/american-standard-code-information-interchange/ascii-codes-table.png"/></td>
 	  <td align="bottom"><img src="https://cdn.meme.am/cache/instances/folder942/61515942.jpg"/></td>
 	  <td align="bottom"><img src="https://cnet1.cbsistatic.com/img/D5BzKihICWRk4OotpApfVdoW3W0=/1600x900/2016/07/08/b23bd00c-7917-4385-b9d2-82cb1aaa708a/pokemon-go-logo.jpg"/></td>
-	  <td align="bottom"><img src=""/></td>
-	  <td align="bottom"><img src=""/></td>
+	  <td align="bottom"><img src="https://www.lebanonstem.org/wp-content/uploads/2016/08/shutterstock_157775207.jpg"/></td>
+	  <td align="bottom"><img src="http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"/></td>
 	  <td align="bottom"><img src=""/></td>
 	  <td align="bottom"><img src=""/></td>
 	  <td align="bottom"><img src=""/></td>
