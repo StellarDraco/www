ab4ffbda7dc291cec480dbe2d751a8b6baa936b3
Kaden Steele <kadenjsteele@gmail.com>
2016-12-02 11:50:16 -0800

    Update pics.php
    Just a test

--- pics.php
+++ pics.php
@@ -4,10 +4,9 @@
 <PRE>
 <?php
 	echo "Type passphrase to open...";
-	if(isset($_GET['string' == "1234"])){
-		$string = $_GET['string'];
+	if(isset($_GET['string' == '1234'])){
 		echo "correct";
-} 	else(isset($_GET['string' === "1234"])){
+} 	else{
 		echo "incorrect, please try again";
 }
 ?>
